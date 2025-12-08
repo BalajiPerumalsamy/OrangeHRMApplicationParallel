0363c8950de2023ebf30ebdd69fa56c27f8c3e29
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2025-12-08 15:48:20 +0530

    New Page class upload to GitHub

---
+++ src/main/java/com/orangeHRMPages/PIM_Page.java
@@ -0,0 +1,78 @@
+package com.orangeHRMPages;
+
+import com.utilsPages.DriverFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PIM_Page extends Base_Page
+{
+    public PIM_Page()
+    {
+        super();
+        PageFactory.initElements(DriverFactory.getDriver(),this);
+    }
+
+    @FindBy(xpath="//a[text()='Add Employee']")
+    WebElement addEmp;
+
+    @FindBy(xpath="//a[text()='Employee List']")
+    WebElement empList;
+
+    @FindBy(xpath="//a[text()='Reports']")
+    WebElement report;
+
+    @FindBy(xpath="//h6[text()='Add Employee']")
+    WebElement addEmployee;
+
+    @FindBy(xpath="//h5[text()='Employee Information']")
+    WebElement employeeList;
+
+    @FindBy(xpath="//h5[text()='Employee Reports']")
+    WebElement employeeReport;
+
+    public void navigateToAddEmployee()
+    {
+        clickButton(addEmp);
+    }
+
+    public void navigateToEmployeeList()
+    {
+        clickButton(empList);
+    }
+
+    public void navigateToReport()
+    {
+        clickButton(report);
+    }
+
+    public String actualAddEmployee()
+    {
+        return addEmployee.getText();
+    }
+
+    public String expectedAddEmployee()
+    {
+        return "Add Employee";
+    }
+
+    public String actualEmployeeList()
+    {
+        return employeeList.getText();
+    }
+
+    public String expectedEmployeeList()
+    {
+        return "Employee Information";
+    }
+
+    public String actualEmployeeReport()
+    {
+        return employeeReport.getText();
+    }
+
+    public String expectedEmployeeReport()
+    {
+        return "Employee Reports";
+    }
+}
